457f5bcfa0e15cc8c457206a262889f1722b965e
alawn_k <alawn_k@qq.com>
2022-06-11 10:51:53 +0800

    lay2vlan

--- mysql_table_query.py
+++ mysql_table_query.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 #
 def link_db():
     username = 'root'
-    password = 'uz8954UZN'
-    host = '119.91.102.106'
+    password = '2222222'
+    host = '1111111'
     port = '3306'
     dbname = 'building_information'
     engine = create_engine(f'mysql+pymysql://{username}:{urlquote(password)}@{host}:{port}/{dbname}')
@@ -23,7 +23,7 @@ def link_db():
 #     res = session.query(table).first()
 
 def workplace_info(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     workplace_info = "select * from workplace_information where project = '%s'" %project
@@ -32,7 +32,7 @@ def workplace_info(project):
     return info_data
 
 def ip_planning(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     ip_planning = "select * from ip_planning where project = '%s' ORDER BY vlan*1" %project
@@ -42,7 +42,7 @@ def ip_planning(project):
 
 
 def endpoint(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     endpoint = "select * from endpoint where project = '%s' ORDER BY floor*1" %project
@@ -51,7 +51,7 @@ def endpoint(project):
     return endpoint_data
 
 def equipment_type(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     equipment_type = "select * from equipment_type_version where project = '%s'" %project
@@ -60,7 +60,7 @@ def equipment_type(project):
     return equipment_data
 
 def core_ip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     ip_info = "select * from ip_planning where func = '核心网段' and project = '%s'" %project
@@ -69,7 +69,7 @@ def core_ip(project):
     return info_data
 
 def cwl_ip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     ip_info = "select * from ip_planning where func = '无线核心管理段' and project = '%s'" %project
@@ -79,7 +79,7 @@ def cwl_ip(project):
 
 
 def ccs_ip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     ip_info = "select * from ip_planning where func = '智能控制管理网' and project = '%s'" %project
@@ -88,7 +88,7 @@ def ccs_ip(project):
     return info_data
 
 def cwl_ip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     ip_info = "select * from ip_planning where func = '无线核心管理段' and project = '%s'" %project
@@ -97,7 +97,7 @@ def cwl_ip(project):
     return info_data
 
 def cwl_ip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     ip_info = "select * from ip_planning where func = '无线核心管理段' and project = '%s'" %project
@@ -106,7 +106,7 @@ def cwl_ip(project):
     return info_data
 
 def deivce_ip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     info = "select * from manage_ip_assignments where project = '%s'" % project
@@ -116,7 +116,7 @@ def deivce_ip(project):
 
 
 def connection(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     info = "select * from connection_relation where project = '%s'" % project
@@ -126,7 +126,7 @@ def connection(project):
 
 
 def mgtip(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     info = "select * from manage_ip_assignments where project = '%s'" % project
@@ -135,7 +135,7 @@ def mgtip(project):
     return connection
 
 def dhcp(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     info = "select * from parameter where project = '%s'" % project
@@ -145,7 +145,7 @@ def dhcp(project):
 
 
 def special_floor(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     info = "select * from endpoint where convergence = 'N' and project = '%s'" %project
@@ -154,7 +154,7 @@ def special_floor(project):
     return connection
 
 def normal_floor(project):
-    db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='building_information')
+    db = pymysql.connect(host='1111111', user='root', password='2222222', database='building_information')
 
     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
     info = "select * from endpoint where convergence = 'Y' and project = '%s'" %project
@@ -162,7 +162,7 @@ def normal_floor(project):
     connection = cursor1.fetchall()
     return connection
 # def parameter(project):
-#     db = pymysql.connect(host='119.91.102.106', user='root', password='uz8954UZN', database='automation')
+#     db = pymysql.connect(host='1111111', user='root', password='2222222', database='automation')
 #     cursor1 = db.cursor(cursor=pymysql.cursors.DictCursor, )
 #     parameter = "select * from parameter where project = '%s'" %project
 #     cursor1.execute(parameter)
